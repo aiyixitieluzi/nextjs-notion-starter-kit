6df774a857379e33f294a200eb4faeb0b5f440e2
Chen <106084724+aiyixitieluzi@users.noreply.github.com>
2024-03-12 18:58:05 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'cbfc3860e6464777bfcd508034ee4cfb?pvs=4',
+  rootNotionPageId: 'cbfc3860e6464777bfcd508034ee4cfb',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
-  domain: 'https://yanzu-wu.notion.site/cbfc3860e6464777bfcd508034ee4cfb?pvs=4',
+  domain: 'https://yanzu-wu.notion.site/cbfc3860e6464777bfcd508034ee4cfb',
   author: 'Chen',
 
   // open graph metadata (optional)

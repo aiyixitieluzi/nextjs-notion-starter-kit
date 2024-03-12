96cba86e7608a2c400114be1beba9b52c46aadb6
Chen <106084724+aiyixitieluzi@users.noreply.github.com>
2024-03-12 19:17:26 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'cbfc3860e6464777bfcd508034ee4cfb',
+  rootNotionPageId: 'none',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: "Chen's world",
-  domain: 'https://yanzu-wu.notion.site/cbfc3860e6464777bfcd508034ee4cfb',
+  domain: 'none',
   author: 'Chen',
 
   // open graph metadata (optional)
